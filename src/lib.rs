18e3d094882f5cb50516d0d55a55e8849ba0e5e5
Spike Grobstein <me@spike.cx>
2019-04-07 17:28:59 -0700

    initial import

---
+++ src/lib.rs
@@ -0,0 +1,45 @@
+use std::error::Error;
+
+use radarr;
+use alamo_movies;
+
+use alamo_movies::film::Film;
+use alamo_movies::cinema::Cinema;
+
+pub fn fetch_all_alamo_films() -> Result<Vec<Film>, Box<dyn Error>> {
+    let cinemas = Cinema::list();
+
+    let films = cinemas.iter()
+        .map(|cinema| {
+            eprintln!("Fetching cinema data for {}", cinema.slug);
+            let data = Cinema::get_calendar_data(&cinema.id).expect("Failed to get data");
+            let (_cinema, films) = Cinema::from_calendar_data(&data).expect("Failed to get films");
+
+            films
+        })
+        .flatten()
+        .filter(|film| {
+            film.show_type.to_lowercase() == "terror tuesday"
+                || film.show_type.to_lowercase() == "weird wednesday"
+        })
+        .collect();
+
+    Ok(films)
+}
+
+pub fn best_matches(term: &str, results: Vec<radarr::SearchResult>) -> Option<Vec<radarr::SearchResult>> {
+    let matches: Vec<radarr::SearchResult> = results.into_iter()
+        .filter(|result| {
+            result.title.to_lowercase() == term.to_lowercase() 
+                || result.alternative_titles.iter()
+                    .any(|title| title.to_lowercase() == term.to_lowercase())
+        })
+        .collect();
+
+    if matches.len() == 0 {
+        return None;
+    }
+
+    Some(matches)
+}
+
---
+++ src/main.rs
@@ -0,0 +1,61 @@
+use radarr;
+use alamo2radarr::*;
+
+
+fn main() {
+    let config = radarr::Config::new_from_env().unwrap();
+    let client = radarr::Client::new(config);
+
+    let health = client.health().expect("Failed to do radarr healthcheck");
+    let root_folder_path = client.root_folder()
+        .expect("Failed to get root folder");
+
+    let root_folder_path = &root_folder_path[0].path;
+
+    eprintln!("Radarr online! (root folder: {})", root_folder_path);
+
+    // now let's fetch all of the alamo movies
+    let films = fetch_all_alamo_films().expect("Failed to get films from ADC");
+
+    eprintln!("Got back {} films", films.len());
+
+    // now search radarr
+    for film in films {
+        if let Ok(Some(results)) = client.search(&film.name) {
+            let results_count = results.len();
+            let best_results = match best_matches(&film.name, results) {
+                Some(r) => r,
+                None => {
+                    eprintln!("Found no results for {}", film.name);
+                    continue;
+                },
+            };
+
+            eprintln!("Got {}/{} results for {}", best_results.len(), results_count, film.name);
+
+            for result in best_results {
+                let mut payload = match radarr::AddMoviePayload::from_movie_response(&result) {
+                    Some(payload) => payload,
+                    None => {
+                        eprintln!("Cannot create movie payload.");
+                        continue;
+                    }
+                };
+                payload.set_search_for_movie(true);
+                payload.set_monitored(true);
+                payload.set_root_folder_path(root_folder_path);
+
+                match client.add_movie(&payload) {
+                    Ok(_) => eprintln!("Added movie: {}", film.name),
+                    Err(error) => {
+                        eprintln!("Failed to add movie: {}: {}", film.name, error);
+                        continue;
+                    }
+                }
+            }
+        } else {
+            eprintln!("Failed to search for {}", film.name);
+        }
+    }
+}
+
